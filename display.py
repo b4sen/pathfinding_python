715d794eb1572b135c43f1341b3871c2aab4d1e1
b4sen <kbasen823@gmail.com>
2019-09-07 09:51:59 +0200

    initial commit, grid and start node works

---
+++ display.py
@@ -0,0 +1,65 @@
+import sys
+import pygame
+
+
+WIDTH = 600
+HEIGHT = 600
+
+# LEFT CLICK -> DRAW START NODE
+# SHIFT CLICK -> DRAW END NODE
+# CTRL CLICK -> DRAW WALL
+
+
+class Display:
+    def __init__(self, width, height):
+        pygame.init()
+        self.white = 255, 255, 255
+        self.scl = 20
+        self.width = width
+        self.height = height
+        self.cols = self.width // self.scl
+        self.rows = self.height // self.scl
+        self.screen = pygame.display.set_mode((width, height))
+        self.grid = [[0 for i in range(self.rows)] for i in range(self.cols)]
+
+    def draw_grid(self):
+        black = 0, 0, 0
+        for col in range(self.cols):
+            var = (col * self.scl) - 1
+            pygame.draw.line(self.screen, black, (var, 0), (var, self.height), 1)
+
+        for row in range(self.rows):
+            var = (row * self.scl) - 1
+            pygame.draw.line(self.screen, black, (0, var), (self.width, var), 1)
+
+    def get_mouse(self):
+        if(pygame.mouse.get_pressed()[0]):
+            x, y = pygame.mouse.get_pos()
+            x = int(x // self.scl)
+            y = int(y // self.scl)
+            self.set_cell(x, y, 1)
+
+    def draw_begin(self):
+        for c in range(self.cols):
+            for r in range(self.rows):
+                if self.grid[c][r] == 1:
+                    pygame.draw.rect(self.screen, (255, 0, 0), (c * self.scl, r * self.scl, self.scl, self.scl))
+
+    def set_cell(self, x, y, val):
+        self.grid[x][y] = val
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.white)
+            self.draw_grid()
+            self.get_mouse()
+            self.draw_begin()
+            pygame.display.flip()
+
+
+d = Display(WIDTH, HEIGHT)
+d.run()
